b91abbf981e5c9973f49632bd60bb4e02f7f3b1b
Umar Farooq <hme465436@gmail.com>
2026-08-17 19:40:09 +0000

    Try fixing this error: `Runtime ReferenceError: useMemo is not defined.

--- src/app/html-to-url/page.tsx
+++ src/app/html-to-url/page.tsx
@@ -1,7 +1,6 @@
-
 "use client"
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { 
   FileCode, 
   Link as LinkIcon, 
